5dd1efc2841d85170cb11fecbfd373a693b26839
Nelson <neldecas12@gmail.com>
2023-09-26 11:13:24 -0500

    custom action for s3 bucket

--- .github/actions/deploy-s3/main.js
+++ .github/actions/deploy-s3/main.js
@@ -4,6 +4,15 @@ const exec = require("@actions/exec");
 
 
 function run() {
+
+    const bucket = core.getInput("bucket", { required: true });
+    const bucket_region = core.getInput("bucket-region", { required: true });
+    const dist = core.getInput("dist-folder", { required: true });
+
+    const s3uri = `s3://${bucket}`;
+
+    exec.exec(`aws s3 sync ${dist} ${s3uri} --region ${bucket_region}`)
+
     core.notice("This is a message from my js custom action")
 }
 
